738a0808c17be26945216e0d753907fb9b224437
onao13 <khovratovic15@yandex.ru>
2023-07-28 13:29:40 +0300

    added a document parser

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main(){
         String::from("resources/test.html"),
         String::from("resources/test.css")
     ]);
-    let parser = parser_page::parser::new_parser(sources);
+    let parser = document_parser::parser::new_parser(sources);
     let page = parser.parse();
     page.print_res();
 }
