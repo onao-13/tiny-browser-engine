337ec2aa7cbbeeb18a1d2365bc6cc4590ec1be3a
onao13 <khovratovic15@yandex.ru>
2023-07-28 13:20:16 +0300

    added a parse documents

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use std::fs::File;
-use std::io::{BufRead, Read};
-use html::html::parse_html;
-use css_v2_1::css::parse_css;
-
 fn main(){
-    // let html = "resources/test.html";
-    //
-    // let mut input = File::open(html).expect("Error find file");
-    // let mut content = String::new();
-    // input.read_to_string(&mut content).expect("Error write to string");
-    //
-    // let node = parse_html(content);
-    // println!("{:#?}", node);
-
-    let css = "resources/test.css";
-
-    let mut input_css = File::open(css).expect("Error find file");
-    let mut content_css = String::new();
-    input_css.read_to_string(&mut content_css).expect("Error write to string");
-
-    let stylesheet = parse_css(content_css);
-    println!("{:#?}", stylesheet)
+    let sources = Vec::from([
+        String::from("resources/test.html"),
+        String::from("resources/test.css")
+    ]);
+    let parser = parser_page::parser::new_parser(sources);
+    let page = parser.parse();
+    page.print_res();
 }
